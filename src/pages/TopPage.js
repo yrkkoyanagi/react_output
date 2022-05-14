3d30a8ae3ca23f04d221d0134a84bf8e1b72667d
yrkkoyanagi <yrkkoyanagi@gmail.com>
2022-05-14 23:21:54 +0900

    割り勘できるけど消えちゃう

--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -1,30 +1,50 @@
 import React from 'react'
+import { createRoot } from 'react-dom/client'
 import PersonForm from '../components/PersonForm'
 import CalcButton from '../components/CalcButton'
+import {Box} from  '@material-ui/core';
+import {TextField} from '@material-ui/core';
 
 export default class TopPage extends React.Component{
 
 	constructor(props){
 		super(props)
 		this.state = {
-			count: 0
+			count: 0,
+			totalAmount: '',
+			partySize: ''
 		}
 		this.addCount = this.addCount.bind(this)
+		this.handleChange = this.handleChange.bind(this)
 	}
+	
+	handleChange (e) {
+		let name = e.target.name;
+		this.setState({[name] : e.target.value})
+	};
 	addCount(){
 		this.setState((state) => {
 			return {
-				count : state.count +1 
+				count : state.totalAmount/state.partySize
 			}
 		})
 	}	
 
     render() {
         return (
-            <div>
+             <Box
+      			component="form"
+      			sx={{
+        		'& > :not(style)': { m: 1, width: '25ch' },
+      			}}
+     			 noValidate
+      			autoComplete="off"
+   			 >
+   			    <TextField name="totalAmount" value={this.state.totalAmount} onChange={this.handleChange} label="合計金額" variant="outlined" />
+            	<TextField name="partySize" value={this.state.partySize} onChange={this.handleChange} label="人数" variant="outlined" />
                 <PersonForm count={this.state.count} />
                 <CalcButton onAddCount={this.addCount} />
-            </div>
+            </Box>
         )
     }
 }
