783c7ea8fd2b1d445edbe85b0daa88d6462f3fa7
yrkkoyanagi <yrkkoyanagi@gmail.com>
2022-05-14 23:18:26 +0900

    とりあえず割り勘できるようになった

--- src/components/PersonForm.js
+++ src/components/PersonForm.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class PersonForm extends React.Component {
     render() {
         return (
-            <div>カウント: { this.props.count}</div>
+            <div>一人当たりの金額 : { this.props.count}</div>
         ) 
     }
 }
